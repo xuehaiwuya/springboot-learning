deed261ceb97688f8782f07e07083fb178ef57fb
leslie <panxiang_work@163.com>
2019-04-26 14:49:50 +0800

    '初始化项目'

---
+++ src/main/java/com/studyinghome/Application.java
@@ -0,0 +1,19 @@
+package com.studyinghome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * SpringbootApplication
+ *
+ * @author Leslie
+ * @email panxiang_work@163.com
+ * @create 2019/4/26 13:46
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
